cae4fe6f7ac9fc0f2b885f5ab12c0777e5e292a4
csursprung <githubOpensesame18>
2016-03-08 21:06:44 +0100

    .

--- app/src/main/java/hu/ursprung/socialnetworkloginsample/LoginActivity.java
+++ app/src/main/java/hu/ursprung/socialnetworkloginsample/LoginActivity.java
@@ -1,8 +1,12 @@
 package hu.ursprung.socialnetworkloginsample;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.content.IntentSender;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -14,28 +18,48 @@ import com.facebook.GraphRequest;
 import com.facebook.GraphResponse;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
-import com.squareup.picasso.Picasso;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.drive.Drive;
+import com.google.android.gms.plus.Plus;
 
-import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.Arrays;
 import java.util.List;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener, GoogleApiClient.ConnectionCallbacks {
+
+    private static final String TAG = LoginActivity.class.getSimpleName();
 
     private TextView info;
     private ImageView imageView;
 
     //facebook login
+    private static final int RC_FACEBOOK_LOGIN = 64206;
     private LoginButton loginButton;
     private CallbackManager callbackManager;
+
+    //google
+    private static final int RC_SIGN_IN = 9001;
+    private GoogleApiClient mGoogleApiClient;
+    private SignInButton signInButton;
+    private ProgressDialog mProgressDialog;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        FacebookSdk.sdkInitialize(getApplicationContext());
+        FacebookSdk.sdkInitialize(getApplicationContext(), RC_FACEBOOK_LOGIN);
         callbackManager = CallbackManager.Factory.create();
         setContentView(R.layout.activity_login);
 
@@ -43,9 +67,101 @@ public class LoginActivity extends AppCompatActivity {
         imageView = (ImageView) findViewById(R.id.imageView);
 
         initFacebookLoginButton();
-
+        initGoogleLogin();
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
     }
 
+    @Override
+    public void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (mGoogleApiClient == null) {
+            mGoogleApiClient = new GoogleApiClient.Builder(this).addApi(Plus.API).addApi(Drive.API).addScope(Drive.SCOPE_FILE).addScope(Drive.SCOPE_APPFOLDER) // required for App Folder sample
+                    .addConnectionCallbacks(this).addOnConnectionFailedListener(this).build();
+        }
+        //mGoogleApiClient.connect();
+    }
+
+    @Override
+    protected void onPause() {
+        if (mGoogleApiClient != null) {
+            mGoogleApiClient.disconnect();
+        }
+        super.onPause();
+    }
+
+    //START google login
+    private void initGoogleLogin() {
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build();
+
+        mGoogleApiClient = new GoogleApiClient.Builder(this).addApi(Plus.API).addApi(Drive.API).addScope(Drive.SCOPE_FILE).addScope(Drive.SCOPE_APPFOLDER) // required for App Folder sample
+                .addConnectionCallbacks(this).addOnConnectionFailedListener(this).build();
+
+
+        signInButton = (SignInButton) findViewById(R.id.sign_in_button);
+        signInButton.setSize(SignInButton.SIZE_WIDE);
+        signInButton.setScopes(gso.getScopeArray());
+        signInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                googleSignIn();
+            }
+        });
+    }
+
+    private void googleSignIn() {
+        /*Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
+        startActivityForResult(signInIntent, RC_SIGN_IN);*/
+        mGoogleApiClient.connect();
+    }
+
+    private void handleSignInResult(GoogleSignInResult result) {
+        Log.d(TAG, "handleSignInResult:" + result.isSuccess());
+        if (result.isSuccess()) {
+            // Signed in successfully, show authenticated UI.
+            GoogleSignInAccount acct = result.getSignInAccount();
+            //mStatusTextView.setText (getString (R.string.signed_in_fmt, acct.getDisplayName ()));
+            info.setText(getString(R.string.signed_in_fmt) + acct.getDisplayName());
+        } else {
+            // Signed out, show unauthenticated UI.
+            info.setText(R.string.signed_in_err);
+        }
+    }
+
+    private void revokeAccess() {
+       /* Auth.GoogleSignInApi.revokeAccess(mGoogleApiClient).setResultCallback(new ResultCallback<Status>() {
+            @Override
+            public void onResult(Status status) {
+                // [START_EXCLUDE]
+                Toast.makeText(getApplicationContext(), "Login failed", Toast.LENGTH_LONG);
+                // [END_EXCLUDE]
+            }
+        });*/
+    }
+
+    private void showProgressDialog() {
+        if (mProgressDialog == null) {
+            mProgressDialog = new ProgressDialog(this);
+            mProgressDialog.setMessage(getString(R.string.loading));
+            mProgressDialog.setIndeterminate(true);
+        }
+
+        mProgressDialog.show();
+    }
+
+    private void hideProgressDialog() {
+        if (mProgressDialog != null && mProgressDialog.isShowing()) {
+            mProgressDialog.hide();
+        }
+    }
+    //END google login
+
+    //START facebook login
     private void initFacebookLoginButton() {
         List<String> readPermissionNeeds = Arrays.asList("user_photos", "email", "user_birthday", "public_profile");
         loginButton = (LoginButton) findViewById(R.id.login_button);
@@ -58,14 +174,15 @@ public class LoginActivity extends AppCompatActivity {
                         new GraphRequest.GraphJSONObjectCallback() {
                             @Override
                             public void onCompleted(JSONObject object, GraphResponse response) {
-                                try {
+                                openDetailActivity();
+                                /*try {
                                     info.setText("Hi, " + object.getString("name"));
                                     object.getJSONObject("picture").getJSONObject("data").getString("url");
                                     String userId = object.getString("id");
                                     Picasso.with(getApplicationContext()).load("https://graph.facebook.com/" + object.getString("id") + "/picture?type=large").into(imageView);
                                 } catch (JSONException ex) {
                                     ex.printStackTrace();
-                                }
+                                }*/
                             }
                         });
                 Bundle parameters = new Bundle();
@@ -81,7 +198,7 @@ public class LoginActivity extends AppCompatActivity {
 
             @Override
             public void onError(FacebookException e) {
-                info.setText("Login attempt failed.");
+                info.setText(R.string.signed_in_err);
             }
         });
     }
@@ -89,6 +206,50 @@ public class LoginActivity extends AppCompatActivity {
     //facebook login
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        callbackManager.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == RC_FACEBOOK_LOGIN) {
+            callbackManager.onActivityResult(requestCode, resultCode, data);
+        }
+        if (requestCode == RC_SIGN_IN && resultCode == RESULT_OK) {
+            mGoogleApiClient.connect();
+
+        }
+    }
+
+    //google login
+    @Override
+    public void onConnectionFailed(ConnectionResult result) {
+        Log.i(TAG, "GoogleApiClient connection failed: " + result.toString());
+        if (!result.hasResolution()) {
+            // show the localized error dialog.
+            GoogleApiAvailability.getInstance().getErrorDialog(this, result.getErrorCode(), 0).show();
+            return;
+        }
+        try {
+            result.startResolutionForResult(this, RC_SIGN_IN);
+        } catch (IntentSender.SendIntentException e) {
+            Log.e(TAG, "Exception while starting resolution activity", e);
+        }
+    }
+
+    //google login, callback method
+    @Override
+    public void onConnected(Bundle bundle) {
+        Log.i(TAG, "GoogleApiClient connected");
+    }
+
+    //google login, callback method
+    @Override
+    public void onConnectionSuspended(int i) {
+        Log.i(TAG, "GoogleApiClient connection suspended");
+    }
+
+    private void openDetailActivity() {
+        Intent detailActivity = new Intent(this, DetailActivity.class);
+        startActivity(detailActivity);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
     }
 }
